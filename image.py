350328fa59a3a6c204acc1a207a780cfc2936fdc
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-15 12:35:51 -0800

    finsihed draw method

--- image.py
+++ image.py
@@ -7,9 +7,15 @@ class Image:
         self.image = None  # Make this a pygame.Surface with text on it saying circlify
         self.circles = []
         self.circle_spawn_rate = 1  # Lower for higher resolution.
+        self.active = True
 
     def update(self, window):
         self.draw(self, window)
 
     def draw(self, window):
-        pass
+        if self.active:
+            for circle in self.circles:
+                circle.draw(window)
+
+        else:
+            window.blit(pygame.transform.scale(self.image, (self.width, self.height)), (self.x, self.y))
