7b95574339566f20c4c988129d0b159ec5b47ef0
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-14 23:15:23 -0800

    add draw method

--- circle.py
+++ circle.py
@@ -3,12 +3,14 @@ pygame.init()
 
 
 class Circle:
-    def __init__(self, x, y):
+    def __init__(self, x, y, color):
         self.x, self.y = x, y
+        self.radius = 0
+        self.color = color
         self.grow_speed = 1  # 0 if not growing
 
     def update(self, window):
         self.draw(window)
 
     def draw(self, window):
-        pass
+        pygame.draw.circle(window, self.color, (self.x, self.y), self.radius)
