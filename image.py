f96a041eaa86785f88d07833d3d5a06823ed663f
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-14 23:51:10 -0800

    add image attrs

--- image.py
+++ image.py
@@ -4,6 +4,9 @@ import pygame
 class Image:
     def __init__(self, x, y, width, height):
         self.x, self.y, self.width, self.height = x, y, width, height
+        self.image = None  # Make this a pygame.Surface with text on it saying circlify
+        self.circles = []
+        self.circle_spawn_rate = 1  # Lower for higher resolution.
 
     def update(self, window):
         self.draw(self, window)
