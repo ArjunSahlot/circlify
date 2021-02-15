1bda49fba776370ac86bc5f64f5c959de34be803
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-15 14:37:17 -0800

    bug fixes

--- image.py
+++ image.py
@@ -15,7 +15,7 @@ class Image:
         text = pygame.font.SysFont("comicsans", 120).render("CIRCLIFY", 1, WHITE)
         image.blit(text, (width//2 - text.get_width()//2, height//2 - text.get_height()//2))
 
-        self.open = None
+        self.open: list = None
         self.circles: "list[Circle]" = []
         self.circle_spawn_rate = 1  # Lower for higher resolution.
         self.showing_image = False
@@ -41,17 +41,17 @@ class Image:
 
     def update(self, window):
         self.draw(window)
-        if self.growing:
+        if self.growing and not self.showing_image:
             self.spawn()
             for circle in self.circles:
                 circle.grow()
 
     def spawn(self):
         for _ in range(self.circle_spawn_rate):
-            x, y = random.randrange(self.width), random.randrange(self.height)
+            x, y = self.open.pop(random.randrange(len(self.open)))
             attempts = 0
             while self.in_circles((x + self.x, y + self.y)):
-                x, y = random.randrange(self.width), random.randrange(self.height)
+                x, y = self.open.pop(random.randrange(len(self.open)))
                 attempts += 1
                 if attempts > 1000:
                     self.growing = False
