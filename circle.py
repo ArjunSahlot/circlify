b5668b798dbb99e0391bc6944a5dfa0318d2d28e
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-14 22:28:59 -0800

    base methods for circle

--- circle.py
+++ circle.py
@@ -3,5 +3,12 @@ pygame.init()
 
 
 class Circle:
-    def __init__(self):
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+        self.grow_speed = 1  # 0 if not growing
+
+    def update(self, window):
+        self.draw(window)
+
+    def draw(self, window):
         pass
