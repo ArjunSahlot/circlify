30a29b435f202285a619e7b9891d3c3aa97fff1f
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-14 23:25:27 -0800

    base init

--- image.py
+++ image.py
@@ -2,5 +2,5 @@ import pygame
 
 
 class Image:
-    def __init__(self):
-        pass
+    def __init__(self, x, y, width, height):
+        self.x, self.y, self.width, self.height = x, y, width, height
