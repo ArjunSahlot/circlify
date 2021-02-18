6f7a69ea20abfee23d9d8de4a36c549ff26c435c
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-17 23:42:09 -0800

    added export button

--- interface.py
+++ interface.py
@@ -10,7 +10,9 @@ class Interface:
     x, y = HEIGHT, 0
     image = Image(start_x, start_y, HEIGHT, HEIGHT)
     imp = Button(WIDTH - 5 - 150, 5, 150, text="Import", border=5)
+    exp = Button(imp.x, imp.y+imp.height + 5, imp.width, text="Export", border=imp.border)
 
     def update(self, window, events):
         self.image.update(window)
         self.imp.update(window)
+        self.exp.update(window)
