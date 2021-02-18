d593bdebe2534197a3e1aa94a4c5e909a6711a78
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-17 23:24:24 -0800

    interface method structure

--- interface.py
+++ interface.py
@@ -8,3 +8,9 @@ class Interface:
     start_x = start_y = 0
     im_size = HEIGHT
     x, y = HEIGHT, 0
+
+    def update(self, window, events):
+        self.draw(window)
+
+    def draw(self, window):
+        pass
