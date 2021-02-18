8784088d018cb654b38578c574ec180bb084777c
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-17 23:39:09 -0800

    add import button

--- interface.py
+++ interface.py
@@ -9,6 +9,8 @@ class Interface:
     start_x = start_y = 0
     x, y = HEIGHT, 0
     image = Image(start_x, start_y, HEIGHT, HEIGHT)
+    imp = Button(WIDTH - 5, 5, 150, text="Import", border=5)
 
     def update(self, window, events):
         self.image.update(window)
+        self.imp.update(window)
