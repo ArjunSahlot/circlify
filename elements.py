eb9d042bb25862743d6ebc0d98757d66e4b4f858
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-17 23:09:07 -0800

    finish button

--- elements.py
+++ elements.py
@@ -2,5 +2,46 @@ import pygame
 
 
 class Button:
-    def __init__(self):
-        pass
+    def __init__(self, x, y, width, height=50, text="Button", border=0, layers=1):
+        self.colors = {
+            "bg": (0,) * 3,
+            "border": (255,) * 3,
+            "text": (255,) * 3,
+            "highlight": (100, 100, 100),
+        }
+        self.x, self.y, self.width, self.height = x, y, width, height
+        self.text = text
+        if layers == 1:
+            self.font = pygame.font.SysFont("comicsans", height-10)
+        else:
+            self.font = pygame.font.SysFont("comicsans", height*5//9)
+            self.padding = height/5
+        self.border = border
+        self.layers = layers
+
+    def update(self, window, events=None):
+        self.draw(window)
+
+    def clicked(self, events):
+        if self.hovered():
+            for event in events:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    return event.button == 1
+
+    def draw(self, window):
+        pygame.draw.rect(window, self.colors["highlight" if self.hovered() else "bg"], (self.x, self.y, self.width, self.height))
+        if self.border: pygame.draw.rect(window, self.colors["border"], (self.x, self.y, self.width, self.height), self.border, border_radius=1)
+        if self.layers == 1:
+            text = self.font.render(self.text, 1, self.colors["text"])
+            loc = (self.x + self.width/2 - text.get_width()/2, self.y + self.height/2 - text.get_height()/2)
+            window.blit(text, loc)
+        else:
+            text1, text2 = self.text.split(" ", 1)
+            t1 = self.font.render(text1, 1, self.colors["text"])
+            t2 = self.font.render(text2, 1, self.colors["text"])
+            window.blit(t1, (self.x + self.width/2 - t1.get_width()/2, self.padding))
+            window.blit(t2, (self.x + self.width/2 - t2.get_width()/2, self.padding + t1.get_height()))
+
+    def hovered(self):
+        mx, my = pygame.mouse.get_pos()
+        return self.x <= mx <= self.x + self.width and self.y <= my <= self.y + self.height
