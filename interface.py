cdf94ee393ef2ff4d96632f0f9aa82b1698c977e
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-17 23:40:59 -0800

    bug fix

--- interface.py
+++ interface.py
@@ -9,7 +9,7 @@ class Interface:
     start_x = start_y = 0
     x, y = HEIGHT, 0
     image = Image(start_x, start_y, HEIGHT, HEIGHT)
-    imp = Button(WIDTH - 5, 5, 150, text="Import", border=5)
+    imp = Button(WIDTH - 5 - 150, 5, 150, text="Import", border=5)
 
     def update(self, window, events):
         self.image.update(window)
