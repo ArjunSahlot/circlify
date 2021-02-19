d5c07be2d13ce333ccf8beb26427de5e89730d73
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-18 19:42:24 -0800

    resize buttons

--- interface.py
+++ interface.py
@@ -9,8 +9,8 @@ class Interface:
     start_x = start_y = 0
     x, y = HEIGHT, 0
     image = Image(start_x, start_y, HEIGHT, HEIGHT)
-    imp = Button(WIDTH - 5 - 150, 5, 150, text="Import", border=5)
-    exp = Button(imp.x, imp.y+imp.height + 5, imp.width, text="Export", border=imp.border)
+    imp = Button(WIDTH - 5 - 225, 5, 225, 65, "Import", 5)
+    exp = Button(imp.x, imp.y+imp.height + 5, imp.width, imp.height, "Export", imp.border)
 
     def update(self, window, events):
         self.image.update(window)
