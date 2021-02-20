9fe9fe019e8823c7922d710d9b0fccf9aef2b838
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-19 21:07:43 -0800

    update surface

--- interface.py
+++ interface.py
@@ -11,7 +11,7 @@ class Interface:
     image = Image(start_x, start_y, HEIGHT, HEIGHT)
     imp = Button(WIDTH - 5 - 225, 5, 225, 65, "Import", 5)
     exp = Button(imp.x, imp.y+imp.height + 5, imp.width, imp.height, "Export", imp.border)
-    settings = ImgButton(WIDTH - 5 - 100, HEIGHT - 5 - 100, 100, 100, None, 5)
+    settings = ImgButton(WIDTH - 5 - 100, HEIGHT - 5 - 100, 100, 100, pygame.image.load(os.path.join("assets", "settings_icon.png")), 5)
 
     def update(self, window, events):
         self.image.update(window)
