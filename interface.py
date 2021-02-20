99481b824dde620fb89f6d236c7f4dbf43cf239f
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-19 21:04:17 -0800

    settings var

--- interface.py
+++ interface.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-from elements import Button
+from elements import Button, ImgButton
 from image import Image
 pygame.init()
 
@@ -11,6 +11,7 @@ class Interface:
     image = Image(start_x, start_y, HEIGHT, HEIGHT)
     imp = Button(WIDTH - 5 - 225, 5, 225, 65, "Import", 5)
     exp = Button(imp.x, imp.y+imp.height + 5, imp.width, imp.height, "Export", imp.border)
+    settings = ImgButton(WIDTH - 5 - 100, HEIGHT - 5 - 100, 100, 100, None, 5)
 
     def update(self, window, events):
         self.image.update(window)
