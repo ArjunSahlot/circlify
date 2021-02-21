e958f3eb0b263b003a8518461cbb2c3a6ff47f39
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-20 20:56:28 -0800

    display text

--- interface.py
+++ interface.py
@@ -13,7 +13,7 @@ class Interface:
     x, y = HEIGHT, 0
     image = Image(start_x, start_y, HEIGHT, HEIGHT)
     imp = Button(x + 5, 5, 225, 65, "Import", 5)
-    imp_text = "No file imported"
+    imp_text = "No file imported."
     exp = Button(imp.x, imp.y+imp.height + 5, imp.width, imp.height, "Export", imp.border)
     exp_text = "Latest export: Never"
     settings = ImgButton(WIDTH - 5 - 100, HEIGHT - 5 - 100, 100, 100, pygame.image.load(os.path.join("assets", "settings_icon.png")), 5)
@@ -23,8 +23,15 @@ class Interface:
         self.image.update(window)
         self.imp.update(window)
         if self.imp.clicked(events):
-            self.image.set_image(pygame.image.load(askopenfilename()))
+            try:
+                self.image.set_image(pygame.image.load(askopenfilename()))
+            except Exception as e:
+                self.imp_text = str(e) if "()" not in str(e) else "Unable to import image"
         self.exp.update(window)
         if self.exp.clicked(events):
             pygame.image.save(self.image.render(), asksaveasfilename())
+        t1 = self.font.render(self.imp_text, 1, RED if "." not in self.imp_text else BLACK)
+        window.blit(t1, (self.imp.x + self.imp.width + 10, self.imp.y + self.imp.height/2 - t1.get_height()/2))
+        t2 = self.font.render(self.exp_text, 1, RED if self.exp_text == "Export failed" else BLACK)
+        window.blit(t2, (self.exp.x + self.exp.width + 10, self.exp.y + self.exp.height/2 - t2.get_height()/2))
         self.settings.update(window, events)
