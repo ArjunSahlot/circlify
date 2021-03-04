75f266ae7f31ad35e4b42ca4fe09bdff6cd434c7
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-03-03 23:35:12 -0800

    add line

--- interface.py
+++ interface.py
@@ -184,7 +184,8 @@ class Interface:
             elapsed = self.end_time - self.start_time
             small, large = self.image.get_min_max_circles()
             t1 = self.font.render("Statistics", 1, WHITE)
-            window.blit(t1, (30, HEIGHT - 200))
+            window.blit(t1, (40, HEIGHT - 200))
+            pygame.draw.line(window, WHITE, (5, HEIGHT - 200 + t1.get_height() + 5), (t1.get_width() + 70, HEIGHT - 200 + t1.get_height() + 5), 5)
             stats = [
                 f"Total circles: {num_circles}",
                 f"Smallest circle size: {small}",
@@ -192,10 +193,10 @@ class Interface:
                 f"Time elapsed: {round(elapsed, 3)}",
                 f"FPS: {round(fps, 3)}"
             ]
-            curr = HEIGHT - 200 + t1.get_height() + 15
+            curr = HEIGHT - 200 + t1.get_height() + 20
             for t in stats:
                 text = self.small_font.render(t, 1, WHITE)
-                window.blit(text, (30 + t1.get_width()/2 - text.get_width()/2, curr))
+                window.blit(text, (40 + t1.get_width()/2 - text.get_width()/2, curr))
                 curr += text.get_height() + 5
 
         if self.settings.toggle.checked and self.settings.swap.clicked(events):
